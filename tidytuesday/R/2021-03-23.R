e864f813c685c1ce1e5393738ea763ddb4c2e191
Haoyun <suhaoyun@gmail.com>
2021-03-24 19:16:13 +0800

    Update 2021-03-23.R

--- tidytuesday/R/2021-03-23.R
+++ tidytuesday/R/2021-03-23.R
@@ -1,6 +1,8 @@
-library(tidytuesdayR)
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
 library(lubridate)
+library(tidytuesdayR)
 
 # Import data ------------------
 
@@ -10,7 +12,8 @@ rollcalls <- ttdata$roll_calls
 issues <- ttdata$issues
 
 
-# filter data ------------------
+# join and filter data ------------------
+
 joined <- unvotes %>%
   inner_join(rollcalls, by = "rcid") %>%
   inner_join(issues, by = "rcid")
@@ -18,17 +21,29 @@ joined <- unvotes %>%
 asean <- c("Brunei", "Cambodia", "Indonesia", "Laos", "Malaysia", "Myanmar (Burma)", "Philippines", "Singapore", "Thailand", "Vietnam")
 
 temp <- joined %>%
-  filter(country %in% asean | country == "China",
-         short_name == "hr") %>%
-  group_by(country, year = year(date)) %>%
-  select(rcid, country, year, vote:descr) %>%
+  filter(country %in% asean) %>%
+  group_by(issue, country, year = year(date)) %>%
   summarise(votes = n(),
-            percent_yes = mean(vote == "yes"),
-            percent_abstain = mean(vote == "abstain")) %>%
+            percent_yes = mean(vote == "yes")) %>%
   filter(year >= 2000, votes > 5)
 
+
 # graph --------------------
-ggplot(temp, aes(year, percent_yes)) +
-  geom_point() +
+# ggthemes examples: https://github.com/BTJ01/ggthemes
+
+ggplot(temp, aes(year, percent_yes, color = issue)) +
   geom_smooth(se = FALSE) +
-  facet_wrap(~ country)
+  facet_wrap(~ country, nrow = 2) +
+  labs(title = "How ASEAN countries vote in the UN",
+       #caption = "Data source: github.com/dgrtwo/unvotes",
+       x = "Year",
+       y = "Percent of 'Yes' votes",
+       color = "Issues"
+       ) +
+  scale_x_continuous(breaks = seq(2000, 2020, by = 10)) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  #scale_color_fivethirtyeight() + theme_fivethirtyeight()
+  #scale_colour_wsj("colors6", "") + theme_wsj()
+  scale_color_ptol("issue") + theme_minimal()
+  
+  
